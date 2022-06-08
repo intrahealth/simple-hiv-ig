a182bb3ee4aca1737ed0bad0927b34f2b454cb3b
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2022-06-08 17:27:36 +0200

    wip: easylib troubleshooting

--- _runsimple.sh
+++ _runsimple.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# load data script
-jq --version
-
 # run sep cleanup script
 ./_runcleanup.sh
 
@@ -13,19 +10,20 @@ mv fsh-generated/resources/CodeSystem-* input/vocabulary/codesystem/manual/
 mv fsh-generated/resources/ValueSet-* input/vocabulary/valueset/manual/
 mv fsh-generated/resources/Library-* input/resources/
 
-bash _refresh.sh 
-
+bash _refresh.sh
 bash _genonce.sh -no-sushi 
 
-# convenience
-export FHIR="https://cloud.alphora.com/sandbox/r4/cqm/fhir"
+export FHIR="https://blaze.life.uni-leipzig.de/fhir"
+# export FHIR="https://cloud.alphora.com/sandbox/r4/cqm/fhir"
 # export FHIR="http://hapi.fhir.org/baseR4"
 export HEADER="Content-Type: application/fhir+json"
 
 # PUT
-curl -X PUT -H "$HEADER" --data @Library-FHIR-ModelInfo.json $FHIR/Library/FHIR-ModelInfo | jq .
+# curl -X PUT -H "$HEADER" --data @Library-FHIR-ModelInfo.json $FHIR/Library/FHIR-ModelInfo | jq .
+curl -X PUT -H "$HEADER" --data @output/Device-cqf-tooling.json $FHIR/Device/cqf-tooling | jq .
 
 for FILE in FHIRHelpers FHIRCommon EasyLib ; do curl -X PUT -H "$HEADER" --data @output/Library-${FILE}.json $FHIR/Library/${FILE} | jq . ; done
+# for FILE in FHIRHelpers FHIRCommon EasyLib ; do curl -X PUT -H "$HEADER" --data @output/Library-${FILE}.json $FHIR/Library/${FILE} | jq . ; done
 # for FILE in FHIRHelpers FHIRCommon EasyLib ; do curl -X PUT -H "$HEADER" --data @input/resources/Library-${FILE}.json $FHIR/Library/${FILE} | jq . ; done
 # for FILE in EasyLib ; do curl -X PUT -H "$HEADER" --data @input/resources/Library-${FILE}.json $FHIR/Library/${FILE} | jq . ; done
 
@@ -40,7 +38,7 @@ echo "Exit code: ${?}"
 
 # profit
 curl -H "$HEADER" $FHIR'/Measure/Easy/$evaluate-measure?periodStart=2019-01-01&periodEnd=2022-12-31' | jq . 
-curl -H "$HEADER" $FHIR'/Measure/Easy/$evaluate-measure?periodStart=1960-01-01&periodEnd=2022-12-31&subject=numerator&reportType=subject' | jq . 
+# curl -H "$HEADER" $FHIR'/Measure/Easy/$evaluate-measure?periodStart=1960-01-01&periodEnd=2022-12-31&subject=numerator&reportType=subject' | jq . 
 
 
 # fyi: result for using fhirpath as numerator
